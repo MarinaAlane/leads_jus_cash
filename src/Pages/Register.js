c7ed059b13ba84c46e4596e10057c22f0717f137
Marina Alane <marina.alane@outlook.com>
2024-08-14 18:07:51 -0300

    chore: add styles and yup and formik validation

---
+++ src/Pages/Register.js
@@ -0,0 +1,11 @@
+import RegisterForm from "../Components/RegisterForm";
+
+function Register() {
+  return (
+    <>
+      <RegisterForm />
+    </>
+  );
+}
+
+export default Register;
---
+++ src/Schemas/RegisterFormSchema.js
@@ -0,0 +1,21 @@
+import * as Yup from 'yup';
+
+const RegisterFormSchema = Yup.object({
+  nome: Yup.string()
+    .required('Nome é obrigatório'),
+  email: Yup.string()
+    .email('E-mail inválido')
+    .required('E-mail é obrigatório'),
+  senha: Yup.string()
+    .required('Senha é obrigatória')
+    .min(8, 'Senha deve ter pelo menos 8 caracteres')
+    .matches(/(?=.*[A-Z])/, 'Senha deve conter pelo menos uma letra maiúscula')
+    .matches(/(?=.*[a-z])/, 'Senha deve conter pelo menos uma letra minúscula')
+    .matches(/(?=.*[0-9])/, 'Senha deve conter pelo menos um número')
+    .matches(/(?=.*[@$!%*?&])/, 'Senha deve conter pelo menos um caractere especial'),
+  confirmarSenha: Yup.string()
+    .oneOf([Yup.ref('senha'), null], 'Senhas devem corresponder')
+    .required('Confirmação de senha é obrigatória')
+});
+
+export default RegisterFormSchema;
