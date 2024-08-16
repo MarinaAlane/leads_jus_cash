8ba58d54d9ecd8682fa6285888ba29d897033408
Marina Alane <marina.alane@outlook.com>
2024-08-16 05:30:24 -0300

    chore: add drag and drop

--- src/Pages/Leads/Leads.js
+++ src/Pages/Leads/Leads.js
@@ -1,14 +1,12 @@
 import React, { useState } from 'react';
-
 import logo from '../../Assets/logo.png';
 import NewLeadModal from '../../Components/Modal/NewLeadModal/NewLeadModal';
 import SucessModal from '../../Components/Modal/SucessModal/SucessModal';
 import ErrorModal from '../../Components/Modal/ErrorModal/ErrorModal';
-
 import './Leads.styles.css';
+import LeadsList from '../../Components/List/LeadsList';
 
 function Leads() {
-  const userData = JSON.parse(localStorage.getItem('userFormData'));
   const [newLeadModal, setNewLeadModal] = useState(false);
   const [sucessModal, setSucessModal] = useState(false);
   const [errorModal, setErrorModal] = useState(false);
@@ -35,10 +33,9 @@ function Leads() {
     setNewLeadModal(false);
     setErrorModal(true);
   }
-  const closeErrorModal = () => setErrorModal(false);
 
-  if (!userData) {
-    return <p>Não há dados disponíveis.</p>;
+  const closeErrorModal = () => {
+    setErrorModal(false);
   }
 
   return (
@@ -48,21 +45,17 @@ function Leads() {
         <NewLeadModal
           closeModal={closeNewLeadModal}
           openSucessModal={openSucessModal}
-          openErrorModal={openErrorModal} // Passando `openErrorModal` como prop
+          openErrorModal={openErrorModal}
         />
       )}
       {!newLeadModal && (
         <>
           <button onClick={openNewLeadModal}> + Novo Lead</button>
-          <div className='section-container'>
-            <section className='title-list'>Cliente Potencial</section>
-            <section className='title-list middle-section'>Dados Confirmados</section>
-            <section className='title-list'>Análise do Lead</section>
-          </div>
         </>
       )}
       {sucessModal && <SucessModal closeModal={closeSucessModal} />}
       {errorModal && <ErrorModal closeModal={closeErrorModal} />}
+      <LeadsList />
     </div>
   );
 }
